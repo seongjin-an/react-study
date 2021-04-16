e532c30c9ca7fe4a0118b0e922fd6f231701545a
hive4bee <hive4bee@gmail.com>
2021-04-16 23:57:58 +0900

    counter

--- basic/src/App.tsx
+++ basic/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import MyComponent from "./components/MyComponent";
 import SomeComponent from "./components/MyClassComponent";
+import Counter from "./components/Counter";
 
 function App() {
     return <>
         <MyComponent name={'성진'}/>
         <SomeComponent name={'aa'}  favoriteNumber={10}/>
+        <Counter num={0}/>
     </>
 }
 
---
+++ basic/src/components/Counter.tsx
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+interface CounterProps{
+    num : number
+}
+class Counter extends Component<CounterProps>{
+
+    state = {
+        num: 0
+    }
+
+    render(){
+        const {num} = this.state;
+        return(
+            <div>
+                <h1>{num}</h1>
+                <button onClick={() => {
+                    console.log("...")
+                    this.setState({num: num+1})
+                }}>+1</button>
+            </div>
+        )
+    }
+}
+
+export default Counter
