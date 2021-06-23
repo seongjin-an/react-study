91226cdcb6224fb6a9a97bdbafa83650fe08805c
seongjin-an <seongjin4us@gmail.com>
2021-06-23 23:53:28 +0900

    update function

--- basic/src/components/MyComponent.tsx
+++ basic/src/components/MyComponent.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import styled from "styled-components";
 
 const ComponentMainStyle = styled.div`
@@ -6,7 +6,32 @@ const ComponentMainStyle = styled.div`
     height: 10vw;
     width: 40vw;
 `
+const temp = [1,2,3]
 const MyComponent = (props:{name:any}) => {
-    return <ComponentMainStyle>HI.. {props.name}</ComponentMainStyle>
+    const [value, setValue] = useState<number[]>([])
+    const imsi: number[] = []
+    const [imsi2, setImsi2] = useState<number[]>([])
+    useEffect(()=>{
+        // temp.forEach(num => {
+        //     setValue([...value, num])
+        // })
+        // temp.forEach(i=>imsi.push(i))
+        // console.log("imsi:", imsi)
+        // for(let i of temp){
+        //     setImsi2([...imsi2, i])
+        // }
+        // console.log("imsi2:", imsi2)
+        for(let i=0; i<temp.length; i++){
+            setImsi2((a:number[])=>[...a, temp[i]])
+        }
+
+    },[temp])
+    return <ComponentMainStyle>
+        HI.. {props.name}
+        value
+        {value && value.map(i=><div>{i}</div>)}
+        imsi2
+        {imsi2 && imsi2.map(i=><div>{i}</div>)}
+    </ComponentMainStyle>
 }
 export default MyComponent
