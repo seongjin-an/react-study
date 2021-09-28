03063e5aa7362c9fe63a628a2c3296bb716b30b4
seongjin-an <seongjin4us@gmail.com>
2021-09-29 02:07:25 +0900

    jest

---
+++ blog/front/src/__test__/Auth.spec.tsx
@@ -0,0 +1,46 @@
+import {Provider} from "react-redux";
+import {createStore} from "../modules";
+import {render, waitFor, fireEvent} from "@testing-library/react";
+import {AnyAction, Store} from "@reduxjs/toolkit";
+import {BrowserRouter} from "react-router-dom";
+import React from "react";
+import {LoginPage} from "../pages/login";
+
+
+describe('Auth Component', () => {
+    let store: Store
+    beforeEach(() => {
+        store = createStore()
+    });
+    it('initialize', async () => {
+        const { getByText, getAllByText } = render(
+            <Provider store={store}>
+                <BrowserRouter>
+                    <LoginPage />
+                </BrowserRouter>
+            </Provider>
+        )
+        expect(getByText(/REACTERS/i))
+        expect(getAllByText(/로그인/i))
+        await waitFor(()=>{
+            const inputs = document.querySelectorAll('input')
+            expect(inputs.length).toBe(2)
+        })
+    })
+    it('should add inputs', async () => {
+        const { getByText, container } = render(
+            <Provider store={store}>
+                <BrowserRouter>
+                    <LoginPage />
+                </BrowserRouter>
+            </Provider>
+        )
+        const inputs = container.querySelectorAll('input')
+        expect(inputs.length).toBe(2)
+        await fireEvent.input(inputs[0], 'imsi')
+        await fireEvent.input(inputs[1], '1234')
+        const button = container.querySelector('button')
+        await fireEvent.click(getByText('회원가입'))
+    })
+
+})
