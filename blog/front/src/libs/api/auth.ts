2278609afac07607d684ab630e13c4bfc2ec1315
seongjin-an <seongjin4us@gmail.com>
2021-10-04 20:46:23 +0900

    add - auth api

--- blog/front/src/libs/api/auth.ts
+++ blog/front/src/libs/api/auth.ts
@@ -8,3 +8,5 @@ export const register = ({username, password}:{username:string, password:string}
     return client.post('/api/auth/register', {username, password})
 }
 export const check = () => client.get('/api/auth/check')
+
+export const logout = () => client.post('/api/auth/logout')
