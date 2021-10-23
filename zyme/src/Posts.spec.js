badabb8a4c401c4387ed4abd56d914ac87cdbe8a
seongjin-an <seongjin4us@gmail.com>
2021-10-24 02:16:30 +0900

    enzyme..

--- zyme/src/Posts.spec.js
+++ zyme/src/Posts.spec.js
@@ -1,48 +1,54 @@
 import React from "react";
-import {configure, mount, shallow} from "enzyme";
+import {configure, shallow, mount} from "enzyme";
 import {Provider} from "react-redux";
-import {createStore, rootReducer, rootSaga} from "./modules";
 import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
 import Posts from "./Posts";
 import toJson from "enzyme-to-json";
 import createSagaMiddleware from "@redux-saga/core";
-import {configureStore} from "@reduxjs/toolkit";
-import {render, screen} from "@testing-library/react";
+// import {render, screen} from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect"
+import configureMockStore from 'redux-mock-store'
+import {createStore, rootReducer, rootSaga} from "./modules";
+import {compose, configureStore} from "@reduxjs/toolkit";
 
-let store;
-const setup = (WrappedComponent, props={}) => {
+let store
+const setup2 = (WrappedComponent, props={}) => {
     const wrapper = mount(
         <Provider store={store}>
             <WrappedComponent {...props}/>
-        </Provider>,
-        { context: { store }}
+        </Provider>
     )
     return {wrapper}
 }
+
+const sagaMiddleware = createSagaMiddleware()
+const mockStore = configureMockStore([sagaMiddleware])
 describe('enzyme saga test', () => {
 
     beforeEach(() => {
         // store = createStore()
-        const sagaMiddleware = createSagaMiddleware()
-        const _store = configureStore({
-            reducer: rootReducer,
-            middleware: [sagaMiddleware],
+
+        // const _store = configureStore({
+        //     reducer: rootReducer,
+        //     middleware: [sagaMiddleware],
+        // })
+        // sagaMiddleware.run(rootSaga)
+        // store = _store
+        store = mockStore({
+            post: {
+                list: [
+                    {"userId": 11, "id": 11, "title": 'hello', "body": 'world'}
+                ]
+            }
         })
-        sagaMiddleware.run(rootSaga)
-        store = _store
         configure({ adapter: new Adapter() });
     })
-    it('should match snapshot', async () => {
+    it('should match snapshot', () => {
         // const wrapper = render(<Provider store={store}><Posts/></Provider>)
         // expect(await screen.findByText('sunt aut facere repellat provident occaecati excepturi optio reprehenderit')).toBeInTheDocument()
-        const { wrapper } = await setup(Posts)
+
+
+        const { wrapper } = setup2(Posts)
         expect(toJson(wrapper)).toMatchSnapshot()
-        // console.log('wrapper:', toJson(wrapper.children().children()))
-        // const div = await wrapper.findWhere(node => {
-        //     return node.text()==='sunt aut facere repellat provident occaecati excepturi optio reprehenderit\n' +
-        //         'quia et suscipit suscipit recusandae consequuntur expedita et cum reprehenderit molestiae ut ut quas totam nostrum rerum est autem sunt rem eveniet architecto'
-        // })
-        // console.log('findWhere:', div)
    })
 })
