f79756b3c93cd92e7bf524714205ce81e87c7ae6
seongjin-an <seongjin4us@gmail.com>
2021-10-27 23:44:53 +0900

    enzyme..

--- zyme/src/Posts.spec.js
+++ zyme/src/Posts.spec.js
@@ -1,13 +1,14 @@
 import React from "react";
-import {configure, shallow, mount} from "enzyme";
+import {configure, mount} from "enzyme";
 import {Provider} from "react-redux";
 import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
 import Posts from "./Posts";
 import toJson from "enzyme-to-json";
 import "@testing-library/jest-dom/extend-expect"
 import configureMockStore from 'redux-mock-store'
-import {createStore, rootReducer, rootSaga} from "./modules";
 import * as actions from "./modules/post/postAction";
+import * as redux from 'react-redux'
+import {READ_POST_REQUEST} from "./modules/post/postTypes";
 
 
 let store
@@ -23,16 +24,29 @@ const setup2 = (WrappedComponent, props={}) => {
 const mockStore = configureMockStore()
 store = mockStore()
 describe('enzyme saga test', () => {
-
+    let spyOnUseSelector;
+    let spyOnUseDispatch;
+    let mockDispatch;
     beforeEach(() => {
-        // store = createStore()
 
-        // const _store = configureStore({
-        //     reducer: rootReducer,
-        //     middleware: [sagaMiddleware],
-        // })
-        // sagaMiddleware.run(rootSaga)
-        // store = _store
+
+        // Mock useSelector hook
+        spyOnUseSelector = jest.spyOn(redux, 'useSelector');
+        spyOnUseSelector.mockReturnValue({
+            post: {
+                list: [
+                    {"userId": 11, "id": 11, "title": 'hello', "body": 'world'}
+                ]
+            }
+        });
+        //
+        // // Mock useDispatch hook
+        spyOnUseDispatch = jest.spyOn(redux, 'useDispatch');
+        // Mock dispatch function returned from useDispatch
+        mockDispatch = jest.fn();
+        spyOnUseDispatch.mockReturnValue(mockDispatch);
+
+
         store = mockStore({
             post: {
                 list: [
@@ -42,19 +56,15 @@ describe('enzyme saga test', () => {
         })
         configure({ adapter: new Adapter() });
     })
+    afterEach(() => {
+        jest.restoreAllMocks();
+    });
     it('should match snapshot', () => {
         // const wrapper = render(<Provider store={store}><Posts/></Provider>)
         // expect(await screen.findByText('sunt aut facere repellat provident occaecati excepturi optio reprehenderit')).toBeInTheDocument()
-
-
         const { wrapper } = setup2(Posts)
         expect(toJson(wrapper)).toMatchSnapshot()
    })
-    it('should store', ()=>{
-        store = createStore()
-        const {wrapper} = setup2(Posts)
-        console.log('store.getState()...:', store.getState())
-    })
     it('should match actions', () => {
         const expectedActions = {
             readPostRequest:{
@@ -74,4 +84,18 @@ describe('enzyme saga test', () => {
         expect(actions.readPostSuccess([{userId:1,id:1,title:'hello',body:'world'}])).toEqual(expectedActions.readPostSuccess)
         expect(actions.readPostFailure()).toEqual(expectedActions.readPostFailure)
     });
+
+    it('should spy on useSelector and useDispatch', () => {
+
+        const { wrapper } = setup2(Posts)
+
+        expect(spyOnUseSelector).toBeCalledTimes(1);
+        expect(spyOnUseDispatch).toBeCalledTimes(1);
+        actions.readPostRequest(mockDispatch)
+        expect(mockDispatch.mock.calls.length).toBe(1)
+        expect(mockDispatch.mock.calls[0]).toBe({
+            type: READ_POST_REQUEST,
+            payload: undefined
+        })
+    })
 })
